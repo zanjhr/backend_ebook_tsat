bcf5673f9fb0ac3217cc2b486a28a0b1bb45e1ac
zanjhr <jhrrazan@apps.ipb.ac.id>
2024-07-01 19:54:22 +0700

    update useradmin

--- models/UserAdmin.js
+++ models/UserAdmin.js
@@ -1,19 +1,19 @@
-import { DataTypes } from "sequelize";
-import db from "../config/Database.js";
+// import { DataTypes } from "sequelize";
+// import db from "../config/Database.js";
 
-const UserAdmin = db.define('UserAdmin', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    nik: {
-        type: DataTypes.STRING,
-        unique: true,
-    },
-}, {
-    tableName: 'user_admin', 
-    timestamps: false,
-});
+// const UserAdmin = db.define('UserAdmin', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true,
+//     },
+//     nik: {
+//         type: DataTypes.STRING,
+//         unique: true,
+//     },
+// }, {
+//     tableName: 'user_admin', 
+//     timestamps: false,
+// });
 
-export default UserAdmin;
+// export default UserAdmin;
