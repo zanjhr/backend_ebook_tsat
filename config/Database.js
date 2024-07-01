b23283e51ba2b641688f8966c6d240a479ce49c3
zanjhr <jhrrazan@apps.ipb.ac.id>
2024-07-01 20:33:01 +0700

    update database

--- config/Database.js
+++ config/Database.js
@@ -1,26 +1,14 @@
-import { Sequelize } from "sequelize";
-// const { Sequelize } = require('sequelize');
-
-// const db = new Sequelize({
-//     dialect: "mysql",
-//     host: "localhost",
-//     username: "root",
-//     password: "",
-//     database: 'auth_db'
-// });
-
-// export default db;
+import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 
 const db = new Sequelize({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     dialect: 'mysql',
-    dialectModule: require('mysql2'),
+    dialectModule: mysql2,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
 });
-
-// module.exports = db;
 
 export default db;
