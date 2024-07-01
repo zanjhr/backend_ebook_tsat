a299f4001dc646918c361506cd87370da814a870
zanjhr <jhrrazan@apps.ipb.ac.id>
2024-07-01 19:56:09 +0700

    update useradmin

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import authController from '../controllers/authController.js';
+// import express from 'express';
+// import authController from '../controllers/authController.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-// Definisikan rute untuk autentikasi
-router.post('/auth', authController.authenticateUser);
+// // Definisikan rute untuk autentikasi
+// router.post('/auth', authController.authenticateUser);
 
-export default router;
+// export default router;
