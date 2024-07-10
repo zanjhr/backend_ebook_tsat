50c24bcf77ca0dd7bfaf99fc665e92dfe818fc35
zanjhr <jhrrazan@apps.ipb.ac.id>
2024-07-10 18:27:09 +0700

    update ebook controller, env, dan ebook mw 2

--- config/supabase.js
+++ config/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ogocpvpthdgelmqtyhey.supabase.co'
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nb2NwdnB0aGRnZWxtcXR5aGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2MDg1MjAsImV4cCI6MjAzNjE4NDUyMH0.NDreEwKGDXdxnWSSpIg_sg9gd018NCcOL9wMcgfDuuo
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nb2NwdnB0aGRnZWxtcXR5aGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2MDg1MjAsImV4cCI6MjAzNjE4NDUyMH0.NDreEwKGDXdxnWSSpIg_sg9gd018NCcOL9wMcgfDuuo'
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
