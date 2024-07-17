6943adfe28838fc82a4bfee4b72a7060ea1c4471
zanjhr <jhrrazan@apps.ipb.ac.id>
2024-07-17 20:58:06 +0700

    controller upload foto

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -1,14 +1,6 @@
-import multer from "multer";
+import multer from 'multer';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/homePic"); // Folder untuk menyimpan file yang diunggah
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix + file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage });
 
