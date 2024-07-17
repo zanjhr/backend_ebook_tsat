f5246218ab987816b076b9e31b43a14bc27a75d3
zanjhr <jhrrazan@apps.ipb.ac.id>
2024-07-17 21:24:54 +0700

    controller upload foto

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,47 +1,6 @@
+import { v4 as uuidv4 } from 'uuid';
 import Image from "../models/imageModel.js";
 import supabase from '../config/supabase.js'; // Import your Supabase client
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
-// const { Image } = require('../models/imageModel.js'); // Adjust the path as needed
-
-
-// export const uploadImage = async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//         const { filename } = req.file;
-
-//         if (id) {
-//             const existingImage = await Image.findByPk(id);
-
-//             if (existingImage) {
-//                 // Jika ID ditemukan ganti gambar yang sudah ada
-//                 await existingImage.update({ filename });
-
-//                 return res.status(200).json({
-//                     success: true,
-//                     message: "Image Replaced successfully",
-//                     data: existingImage,
-//                 });
-//             }
-//         }
-
-//         // Jika ID belum ditemukan 
-//         const newImage = await Image.create({ filename });
-
-//         res.status(201).json({
-//             success: true,
-//             message: "Image uploaded successfully",
-//             data: newImage,
-//         });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({
-//             success: false,
-//             message: "Internal Server Error",
-//             error: error.message,
-//         });
-//     }
-// };
 
 export const uploadImage = async (req, res) => {
     try {
@@ -112,7 +71,6 @@ export const uploadImage = async (req, res) => {
     }
 };
 
-
 export const getImageByID = async (req, res) => {
     try {
         const { id } = req.params;
@@ -138,32 +96,3 @@ export const getImageByID = async (req, res) => {
         });
     }
 };
-
-// // Mengambil Gambar berdasarkan ID
-// export const getImageByID = async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//         const image = await Image.findByPk(id, { attributes: ['id', 'filename'] });
-
-//         if (!image) {
-//             return res.status(404).json({
-//                 success: false,
-//                 message: "Image Not Found",
-//             });
-//         }
-
-//         return res.status(200).json({
-//             success: true,
-//             message: "Image Retrieved Successfully",
-//             data: image,
-//         });
-
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({
-//             success: false,
-//             message: "Internal Server Error",
-//             error: error.message,
-//         });
-//     }
-// };
