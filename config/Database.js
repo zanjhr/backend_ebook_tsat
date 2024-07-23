165d388e498849a460604c17f10cf2bfdf7d8c97
zanjhr <jhrrazan@apps.ipb.ac.id>
2024-07-23 14:24:30 +0700

    updatye database

--- config/Database.js
+++ config/Database.js
@@ -1,3 +1,34 @@
+// import { Sequelize } from 'sequelize';
+// import mysql2 from 'mysql2';
+
+// const db = new Sequelize({
+//   username: 'freedb_zanjhr',
+//   password: 'M3H?A6ejzrW99fs',
+//   database: 'freedb_db_ebook',
+//   dialect: 'mysql',
+//   dialectModule: mysql2,
+//   host: 'sql.freedb.tech',
+//   port: 3306,
+//   // dialectOptions: {
+//   //   connectTimeout: 60000, // increase timeout to 60 seconds
+//   // },
+//   logging: console.log,
+// });
+
+// db.authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+// // console.log('DB_HOST:', process.env.DB_HOST);
+// // console.log('DB_USERNAME:', process.env.DB_USERNAME);
+// // console.log('DB_NAME:', process.env.DB_NAME);
+
+// export default db;
+
 import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2';
 
@@ -9,9 +40,9 @@ const db = new Sequelize({
   dialectModule: mysql2,
   host: 'sql.freedb.tech',
   port: 3306,
-  // dialectOptions: {
-  //   connectTimeout: 60000, // increase timeout to 60 seconds
-  // },
+  dialectOptions: {
+    connectTimeout: 60000, // increase timeout to 60 seconds
+  },
   logging: console.log,
 });
 
@@ -22,9 +53,5 @@ db.authenticate()
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
-
-// console.log('DB_HOST:', process.env.DB_HOST);
-// console.log('DB_USERNAME:', process.env.DB_USERNAME);
-// console.log('DB_NAME:', process.env.DB_NAME);
 
 export default db;
